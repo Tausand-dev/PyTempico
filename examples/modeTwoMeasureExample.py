81c3420c72463e9bdcf00a80dab92ec1ae0167bf
David Guzmán <da.guzman@outlook.com>
2024-05-07 12:34:17 -0500

    Create modeTwoMeasureExample.py

---
+++ examples/modeTwoMeasureExample.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""modeTwoMeasureExample
+
+    Created on Tue May  7 11:42 2024
+    
+    Connects to a Tausand Tempico device, changes settings to measure only in
+    channel 1 in mode 2 (125ns-4ms mode). Then, starts a measurement and reads 
+    the results.
+    
+    Instructions: 
+        * before running this example, pyTempico package must be installed.
+        * change 'my_port' to your corresponding port.
+        * connect signals to your Tempico Device. If no signals are measured, 
+        this example will return an empty data array.
+        * run.
+    
+    | @author: David Guzman at Tausand Electronics 
+    | dguzman@tausand.com 
+    | https://www.tausand.com
+"""
+
+import pyTempico
+
+my_port = 'COM16' #change this port to your Tempico device's port
+
+my_device = pyTempico.TempicoDevice(my_port)    #create object
+
+print('opening connection with device in port',my_port)
+my_device.open()             #open connection with device
+if my_device.isOpen():
+    print('connection with my_device is open')
+else:
+    print('connection with my_device is close')
+
+print('\nreseting device. This clears previous measurements, and changes settings to default values.')
+my_device.reset()
+
+#Enable channel 1, disable channels 2-4
+print('\ndisabling channels 2-4.')
+my_device.ch1.enableChannel()   #optional, since enabled by default
+my_device.ch2.disableChannel()
+my_device.ch3.disableChannel()
+my_device.ch4.disableChannel()
+
+print('\nchanging measurement mode in channel 1 to mode 2')
+my_device.ch1.setMode(2)    #default mode is 1, changing to mode 2
+#verify
+print('my_device.ch1.getMode():',my_device.ch1.getMode())
+
+print('\nsending a measure request to device')
+my_device.measure()   #starts a measurement, and saves response in 'data'
+data = my_device.measure()   #starts a measurement, and saves response in 'data'
+print('measured data, in ps:',data)
+
+print('fetch:',my_device.fetch()) #fetch most recent data
+
+my_device.close()            #close connection with device
+if my_device.isOpen():
+    print('connection with my_device is open')
+else:
+    print('connection with my_device is close')
